f576ace2adbc4eccdfb811c7a340d50784b8ffc4
kisiboost <kisiboost@yahoo.co.jp>
2014-01-22 20:04:36 +0900

    Signed-off-by: kisiboost <kisiboost@yahoo.co.jp>

--- chatControl.js
+++ chatControl.js
@@ -3,7 +3,7 @@ var http = require('http');
 var ejs = require('ejs');
 var url = require('url');
 var querystring = require('querystring');
-var settings = require('../setting.js');  // 当ファイルからの相対パス2
+var settings = require('../setting.js');  // 当ファイルからの相対パス3
 
 console.log(settings);
 
@@ -15,7 +15,7 @@ var query;
 function onRequest(req, res) {
 	console.log('------- onRequest -------');
 
-	// パラメータを取得してページ表示2
+	// パラメータを取得してページ表示3
 	if (req.method == 'GET') {
 		console.log('GET!!');
 	  query = url.parse(req.url, true).query;
--- chatRoom - コピー.js
+++ chatRoom - コピー.js
@@ -6,10 +6,10 @@ var server = require.main.exports.server;
 
 var io = require('socket.io').listen(server);
 
-// io.socketsの接続2
+// io.socketsの接続3
 io.sockets.on('connection', function (socket) {
 	var query = require.main.exports.query;
-	// 他ユーザに入室情報を送信2
+	// 他ユーザに入室情報を送信3
 	socket.broadcast.emit('enter', query.userName);
 
 	socket.on('msg', function (data) {
@@ -17,7 +17,7 @@ io.sockets.on('connection', function (socket) {
 	});
 
 	socket.on('exit', function (data) {
-		// 他ユーザに退室情報を送信2
+		// 他ユーザに退室情報を送信3
 		socket.broadcast.emit('exit', query.userName);
 	});
 });
